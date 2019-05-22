397b3cf8e6f91bafef513cdf86bed1f3c62023a0
swq2014 <44500608+swq2014@users.noreply.github.com>
2019-05-22 17:07:30 +0800

    Add files via upload

---
+++ js/newslist.js
@@ -0,0 +1,174 @@
+﻿function gnxw(){//国内新闻
+	news=[
+	"这就是街舞1",
+	"这就是炫舞2",
+	"这就是吃饭3",
+	"这就是街舞4",
+	"这就是街舞5",
+	"这就是街舞6",
+	"这就是街舞7",
+	"这就是街舞8",
+	"这就是街舞9",
+	"这就是街舞10"
+	];
+	link=[
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'"
+	];
+	for(i=0;i<10;i++){
+		document.write("<div class="+'newslist'+">"+"<a href="+link[i]+">"+news[i]+"</a>"+"</div>");
+	}
+}
+function fcxw(){//房产新闻
+	news=[
+	"这就是房产",
+	"这就是房产",
+	"这就是房产",
+	"这就是房产",
+	"这就是房产",
+	"这就是房产",
+	"这就是房产",
+	"这就是房产",
+	"这就是房产",
+	"这就是房产"
+	];
+	link=[
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'"
+	];
+	for(i=0;i<10;i++){
+		document.write("<div class="+'newslist'+">"+"<a href="+link[i]+">"+news[i]+"</a>"+"</div>");
+	}
+}
+function qcxw(){//汽车新闻
+	news=[
+	"这就是汽车",
+	"这就是汽车",
+	"这就是汽车",
+	"这就是汽车",
+	"这就是汽车",
+	"这就是汽车",
+	"这就是汽车",
+	"这就是汽车",
+	"这就是汽车",
+	"这就是汽车"
+	];
+	link=[
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'"
+	];
+	for(i=0;i<10;i++){
+		document.write("<div class="+'newslist'+">"+"<a href="+link[i]+">"+news[i]+"</a>"+"</div>");
+	}
+}
+function kjxw(){//科技新闻
+	news=[
+	"这就是科技",
+	"这就是科技",
+	"这就是科技",
+	"这就是科技",
+	"这就是科技",
+	"这就是科技",
+	"这就是科技",
+	"这就是科技",
+	"这就是科技",
+	"这就是科技"
+	];
+	link=[
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'"
+	];
+	for(i=0;i<10;i++){
+		document.write("<div class="+'newslist'+">"+"<a href="+link[i]+">"+news[i]+"</a>"+"</div>");
+	}
+}
+function lyxw(){//旅游新闻
+	news=[
+	"这就是旅游",
+	"这就是旅游",
+	"这就是旅游",
+	"这就是旅游",
+	"这就是旅游",
+	"这就是旅游",
+	"这就是旅游",
+	"这就是旅游",
+	"这就是旅游",
+	"这就是旅游"
+	];
+	link=[
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'"
+	];
+	for(i=0;i<10;i++){
+		document.write("<div class="+'newslist'+">"+"<a href="+link[i]+">"+news[i]+"</a>"+"</div>");
+	}
+}
+function kfzxw(){//开发者新闻
+	news=[
+	"这就是开发者新闻",
+	"这就是开发者新闻",
+	"这就是开发者新闻",
+	"这就是开发者新闻",
+	"这就是开发者新闻",
+	"这就是开发者新闻",
+	"这就是开发者新闻",
+	"这就是开发者新闻",
+	"这就是开发者新闻",
+	"这就是开发者新闻"
+	];
+	link=[
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'",
+	"'http://www.baidu.com' target='_blank'"
+	];
+	for(i=0;i<10;i++){
+		document.write("<div class="+'newslist'+">"+"<a href="+link[i]+">"+news[i]+"</a>"+"</div>");
+	}
+}
